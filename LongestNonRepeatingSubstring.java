b7b6d643477d52e933e53ebfac535cd743a32363
kamdarkarnavee <kamdarkarnavee@gmail.com>
2019-08-25 11:09:58 -0700

    Returns length of longest non repeating substring

---
+++ LongestNonRepeatingSubstring.java
@@ -0,0 +1,24 @@
+package miscellaneous_problems;
+
+public class LongestNonRepeatingSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int count, maxCount = 0;
+        int i, j = 0;
+        String newString = "";
+        String temp;
+        while(j < s.length()){
+            temp = Character.toString(s.charAt(j));
+            if(newString.contains(temp)){
+                i = newString.indexOf(temp) + 1;
+                newString = newString.substring(i);
+            }
+            newString = newString.concat(temp);
+            count = newString.length();
+            j++;
+            if(maxCount < count){
+                maxCount = count;
+            }
+        }
+        return maxCount;
+    }
+}
